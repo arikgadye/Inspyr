387ef8c023d69116cc85f102b3317122cd82e3ba
Arik Gadye <arikdovgadye@gmail.com>
2014-10-11 23:35:29 -0700

    basic animation working

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 app
-  .controller('MainCtrl', function ($scope, $http, $location) {
-  	$scope.pageClass = 'page-home';
-  });
+.controller('MainCtrl', function ($scope, $rootScope, $http, $location) {
+	var styles = {	
+		front: '.enter-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all;   -webkit-transform:translate3d(100%,0,0)  }  .enter-setup.enter-start {   position:absolute;  -webkit-transform:translate3d(0,0,0)}  .leave-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all;   -webkit-transform:translate3d(0,0,0)} .leave-setup.leave-start {   position:absolute;  -webkit-transform:translate3d(-100%,0,0) };',
+    // appear from left
+    back: '.enter-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all; -webkit-transform:translate3d(-100%,0,0)}  .enter-setup.enter-start {   position:absolute;   -webkit-transform:translate3d(0,0,0) }  .leave-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all;  -webkit-transform:translate3d(0,0,0)} .leave-setup.leave-start {   position:absolute;  -webkit-transform:translate3d(100%,0,0) };'
+  };
+  $scope.direction = function(dir) {
+  	$rootScope.style = styles[dir];
+  };
+  $scope.go = function(path) {
+  	$location.path(path);
+  }
+  $scope.direction('front');
+});
