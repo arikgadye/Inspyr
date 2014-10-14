57903a269a165784d9df047111d1e6c7486afced
Arik Gadye <arikdovgadye@gmail.com>
2014-10-13 21:03:58 -0700

    add better navbar

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -9,23 +9,17 @@ app
 
   $scope.modalOpen = false;
 
-  $scope.signUp = function(){
+  $scope.openModal = function(){
     $scope.modalOpen = true;
+  }
+  $scope.signUp = function() {
+    $location.path('/users')
   }
   $scope.closeModal = function() {
     console.log('clsoing')
     $scope.modalOpen = false;
   }
-	var styles = {	
-		front: '.enter-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all;   -webkit-transform:translate3d(100%,0,0)  }  .enter-setup.enter-start {   position:absolute;  -webkit-transform:translate3d(0,0,0)}  .leave-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all;   -webkit-transform:translate3d(0,0,0)} .leave-setup.leave-start {   position:absolute;  -webkit-transform:translate3d(-100%,0,0) };',
-    // appear from left
-    back: '.enter-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all; -webkit-transform:translate3d(-100%,0,0)}  .enter-setup.enter-start {   position:absolute;   -webkit-transform:translate3d(0,0,0) }  .leave-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all;  -webkit-transform:translate3d(0,0,0)} .leave-setup.leave-start {   position:absolute;  -webkit-transform:translate3d(100%,0,0) };'
-  };
-  $scope.direction = function(dir) {
-  	$rootScope.style = styles[dir];
-  };
-  $scope.go = function(path) {
-  	$location.path(path);
+  $scope.go = function(location) {
+    $location.path(location)
   }
-  $scope.direction('front');
 });
